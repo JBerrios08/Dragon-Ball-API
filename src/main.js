8ae0c10069c357d3db0b3f306835baa9163243ea
Jaime Berrios <bo0310042024@unab.edu.sv>
2024-10-25 12:24:01 -0600

    Agregar datos locales para evitar CORS

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import filtrarPersonajes from "./filterInfo.js";
 
-const URL = "https://cors-anywhere.herokuapp.com/https://dragon-ball-super-api.herokuapp.com/api/characters";
+const URL = "https://api.allorigins.win/get?url=https://dragon-ball-super-api.herokuapp.com/api/characters";
 
 async function getInfo() {
   try {
     const res = await fetch(URL);
     const data = await res.json();
-    filtrarPersonajes(data);
+    const characters = JSON.parse(data.contents);
+    filtrarPersonajes(characters);
   } catch (error) {
     console.error('Error:', error);
   }
